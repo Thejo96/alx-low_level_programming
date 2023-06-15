755926c312192484c622660c7e6c95afc803195d
Thejo96 <thejopodcast2023@gmail.com>
2023-06-15 13:54:49 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
-
-4-puts.c
-
 #include<stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ * /
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
 
