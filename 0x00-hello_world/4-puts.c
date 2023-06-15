f547bd222c82e1a7561d7ee7e8862e262b020373
Thejo96 <thejopodcast2023@gmail.com>
2023-06-15 14:13:01 +0100

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - point "Programming is like building a multilingual 
+ * puzzle, followed bu a newline."
+ * Return 0.
  * /
+ 
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
-
+	 
+		return (0);
+}				
