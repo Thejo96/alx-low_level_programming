a8b15fc08d59d8b4c78fb4e14b0655f90f574f37
Thejo96 <thejopodcast2023@gmail.com>
2023-06-20 13:52:32 +0100

    Messaeg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@ void print_alphabet(void)
 {
 		char letter;
 
-			for (letter = 'a'; letter <= 'z'; letter++)
-						_putchar(letter);
+	        for (letter = 'a'; letter <= 'z'; letter++)
+	       _putchar(letter);
 
-				_putchar('\n');
+		_putchar('\n');
 }
 
