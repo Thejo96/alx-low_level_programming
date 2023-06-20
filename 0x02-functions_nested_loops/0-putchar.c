1fd561f135ad3a256661508dfa4c3eb62bb55a12
Thejo96 <thejopodcast2023@gmail.com>
2023-06-20 13:38:02 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
-int main()
+int main(void)
 {
-	    printf("_putchar");
-	        return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
