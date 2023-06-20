af9ca2c02ae8bd231541e1ef4e0a196a3a8b7bb2
Thejo96 <thejopodcast2023@gmail.com>
2023-06-20 14:12:47 +0100

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,18 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
+		if (n > 0)
+		{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+		} else if (n == 0)
 }
-_putchar(48);
-return (0);
-} else if (n < 0)
-{
-	_putchar('-');
-}
-return (-1);
+		_putchar(48);
+		return (0);
+		} else if (n < 0)
+                {
+		_putchar('-');
+					}
+		return (-1);
 }
 
