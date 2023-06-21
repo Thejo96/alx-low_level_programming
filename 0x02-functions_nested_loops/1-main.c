a81e457d4614d6786defcfb1ae73acc728de0c91
Thejo96 <thejopodcast2023@gmail.com>
2023-06-21 20:43:17 +0100

    1-main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -10,16 +10,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
+        i = 0;
+        /*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+        printf("Infinite loop avoided! \\o/\n");
+        return (0);
 }
-
