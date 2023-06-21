aba92ccc8694ab4da220e9b302302ee1f40be438
Thejo96 <thejopodcast2023@gmail.com>
2023-06-21 20:29:40 +0100

    Debugging

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - causes an infinite loop
+ * Return: 0
  */
+
 int main(void)
 {
-	print_alphabet();
-	return (0);
+	 int i;
+
+	  printf("Infinite loop incoming :(\n");
+
+	   i = 0;
+
+	    /*while (i < 10)*/
+	    /*{*/
+	    /* putchar(i);*/
+	    /*}*/
+
+	    printf("Infinite loop avoided! \\o/\n");
+
+	     return (0);
 }
