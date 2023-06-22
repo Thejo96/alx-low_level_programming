e6750c52bf059f0a321891a49163c6a6a8f71c51
Thejo96 <thejopodcast2023@gmail.com>
2023-06-22 14:58:05 +0100

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,20 +2,21 @@
 
 int main(void)
 {
-	    int i;
+	int i;
 
-	        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-		    i = 0;
+	i = 0;
 
-		        while (i < 10)
-				    {
-					            putchar(i + '0'); // Add '0' to convert the integer to its corresponding ASCII character
-								      //         i++; // Increment i by 1 in each iteration
-								      //             }
-								      //
-								      //                 printf("Infinite loop avoided! \\o/\n");
-								      //
-								      //                     return 0;
-								      //                     }
-								      //                     
+	while (i < 10)
+	{
+		putchar(i + '0'); // Add '0' to convert the integer to its corresponding ASCII character
+		i++; // Increment i by 1 in each iteration
+	}
+				  
+	printf("Infinite loop avoided! \\o/\n");
+				  
+
+	 return 0;
+
+}				   
