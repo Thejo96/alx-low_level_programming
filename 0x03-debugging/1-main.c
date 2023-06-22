7f32bce40d01e14a2a7bb5aca63aecef97fdc9d3
Thejo96 <thejopodcast2023@gmail.com>
2023-06-22 14:54:17 +0100

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-/**
- * main - causes an infinite loop
- * Return: 0
- */
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");
-	i = 0;
+	    int i;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
+	        printf("Infinite loop incoming :(\n");
 
+		    i = 0;
 
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
-}
+		        while (i < 10)
+				    {
+					            putchar(i + '0'); // Add '0' to convert the integer to its corresponding ASCII character
+								      //         i++; // Increment i by 1 in each iteration
+								      //             }
+								      //
+								      //                 printf("Infinite loop avoided! \\o/\n");
+								      //
+								      //                     return 0;
+								      //                     }
+								      //                     
