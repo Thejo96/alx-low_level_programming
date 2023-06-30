2b96b7f0d8050eaa1750957d2a1f1350ada0d89d
Thejo96 <thejopodcast2023@gmail.com>
2023-06-30 18:10:00 +0100

    Hello

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,16 +2,18 @@
 
 int main(void)
 {
-	int n = 0; // Initialsize n to 0
+	int n = 0;  // Initialize n to 0
+		    
 	int a[5];
 	int *p;
-
+		    
 	a[2] = 1024;
 	p = &n;
-	
+		    
 	*(p + 5) = 98;
+		    
+	 printf("a[2] = %d\n", a[2]);
 
-	printf("a[2] = %\n", a[2]);
+	 return 0;
 
-        return 0;
 }
