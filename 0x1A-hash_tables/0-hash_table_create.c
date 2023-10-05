0c79f4b25f7e8b6d69d5ddc6422bcac6c6f33dd3
Thejo96 <thejopodcast2023@gmail.com>
2023-10-05 22:39:00 +0100

    0X1A

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,38 @@
 #include "hash_tables.h"
+#include <stdio.h> // Include for error messages
 
 /**
  * hash_table_create - creates a hash table
  * @size: size of the hash table
  *
- * Return: pointer to hash table
+ * Return: pointer to hash table, or NULL on failure
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+    hash_table_t *table;
 
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+    {
+        fprintf(stderr, "Error: Hash table size cannot be zero.\n");
+        return (NULL);
+    }
 
-	table = calloc(1, sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
+    table = calloc(1, sizeof(hash_table_t));
+    if (table == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for hash table.\n");
+        return (NULL);
+    }
 
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+    table->size = size;
+    table->array = calloc(size, sizeof(hash_node_t *));
+    if (table->array == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for array in hash table.\n");
+        free(table); // Clean up previously allocated memory
+        return (NULL);
+    }
+
+    return (table);
 }
