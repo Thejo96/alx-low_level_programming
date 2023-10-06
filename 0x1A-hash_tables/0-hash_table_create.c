f5d429f44a6ad4feee8eb8307836b9f87b4bef44
Thejo96 <thejopodcast2023@gmail.com>
2023-10-06 14:33:59 +0100

    Updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,20 +18,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    unsigned long int hsh_tb;
+	hash_table_t *hash_table;
+	unsigned long int hsh_tb;
 
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return (NULL);
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t *) * size); // Change this line
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
-    for (hsh_tb = 0; hsh_tb < size; hsh_tb++)
-        hash_table->array[hsh_tb] = NULL;
-    return (hash_table);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (hsh_tb = 0; hsh_tb < size; hsh_tb++)
+		hash_table->array[hsh_tb] = NULL;
+	return (hash_table);
 }
+
